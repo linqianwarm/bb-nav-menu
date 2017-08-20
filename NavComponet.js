696288c4afd61a13baa148a562ee965f5d4ca3f9
linqian <linqian@linqian.local>
2017-08-20 17:31:42 +0800

    fix

---
+++ NavComponet.js
@@ -0,0 +1,104 @@
+
+var Nav = function(config){
+	var _this = this;
+	_this.config = config;
+
+	_this.init();
+}
+
+Nav.prototype = {
+	tplTitleNode: function(nodeData){
+		return '<li class="item-group">'+
+					'<div class="menu-title" data-index='+nodeData.key+'>'+nodeData.name+'>> </div>'+
+					'<ul class="hide" id=ulIndex'+nodeData.key+'></ul>'+
+				'</li>';
+	},
+	tplLeafNode: function(nodeData){
+		return '<li class="leaf-node" data-index='+nodeData.key+'>'+nodeData.name+'</li>';
+	},
+	init: function(){
+		var _this = this,
+				config = _this.config,
+				hashKey = _this.getCurHash();
+
+		_this.bindEvents();
+		_this.render(config.data);
+  	_this.renderSelected(hashKey);
+
+	},
+	render: function () {
+		var _this = this,
+			config = _this.config,
+			data = config.data;
+
+		$("#"+config.container+"").append('<ul id="ulIndex0"></ul>');
+
+		for(var i in data) {
+		  var firstNode = data[i];
+	    if (firstNode) {     
+	      if (firstNode.children && firstNode.children.length > 0) {
+	        $('#ulIndex0').append(_this.tplTitleNode(firstNode));
+	        _this.deepRender(firstNode);
+	      }else {
+	      	$("#ulIndex0").append(_this.tplLeafNode(firstNode));
+	      }
+			}
+		}
+	},
+
+	deepRender: function(data) {
+		var _this = this;
+		if (data.children) {
+	    for(var i in data.children) {
+	      var nextData = data.children[i];
+	      if (nextData.children && nextData.children.length > 0) {
+	        $('#ulIndex'+data.key+'').append(_this.tplTitleNode(nextData));
+	        _this.deepRender(nextData);
+	      }else{
+	        $('#ulIndex'+data.key+'').append(_this.tplLeafNode(nextData));
+	      }
+	    }
+	  }
+	},
+
+	renderSelected: function (key) {
+		$('.leaf-node').removeClass('cur');
+  	$('.leaf-node[data-index='+key+']').addClass('cur');
+  	$('.leaf-node[data-index='+key+']').parentsUntil('#menu','.item-group ul').removeClass('hide');
+  },
+
+  getCurHash: function (){
+  	var hash = window.location.hash;
+	  var hashKey = hash.substr(1);
+	  console.log("hashchange",hashKey);
+	  return hashKey;
+  },
+
+  bindEvents: function (argument) {
+		var _this = this,
+				config = _this.config;
+
+  	$('#'+config.container+'').on('click', '.item-group div', function() {
+	    var $this = $(this);
+	    $this.next().toggleClass('hide');
+	    $this.parent().siblings().children('ul').eq(0).addClass('hide');
+	    console.log($this.text());
+	  })
+	  
+	  $('#'+config.container+'').on('click', '.leaf-node', function() {
+	    var $this = $(this);
+	    var indexKey = $this.data('index');
+
+	    // 此时已改变状态 
+	    window.history.pushState(null,null,"#"+indexKey+"");
+	    _this.renderSelected(indexKey);  
+	  })
+
+	  window.addEventListener('hashchange', function() {
+	    var hashKey = getCurHash();
+	    _this.renderSelected(hashKey);
+	  });
+  }
+}
+
+window.Nav = Nav;
--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # navComponent
 navigation
+
+###预览方式
+```
+	npm install
+	live-server
+```
+
+### 使用方式
+```JavaScript
+new Nav({
+    container: 'menu', //容器ID名
+    data: data //导航栏数据
+})
+
+```
+
+### 导航栏数据需按照此格式传入(层级不限)
+```Json
+[
+  {
+    "name": "一级菜单1",
+    "key": "a",
+    "children": [
+      {
+        "name": "二级菜单1-1",
+        "key": "a-1",
+        "children": [
+          {
+            "name": "三级菜单1-2-1",
+            "key": "a-2-1"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "一级菜单2",
+    "key": "b",
+    "children": [
+      {
+        "name": "二级菜单2-1",
+        "key": "b-1"
+      },
+      {
+        "name": "二级菜单2-2",
+        "key": "b-2"
+      }
+    ]
+  },
+   {
+    "name": "一级菜单3 ",
+    "key": "c"
+  }
+```
